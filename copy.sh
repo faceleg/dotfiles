f86456ed56b226ee792e349b5e2bd9e40c20d92b
Michael Robinson <mike@panmedia.co.nz>
2013-02-12 11:16:06 +1300

    Updated copy script

--- copy.sh
+++ copy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Copy dot files to home
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cp -r $DIR/.[a-zA-Z0-9]* ~/
+for FILE in .zshrc .tmux.conf .vimrc; do
+    cp $FILE ~/
+done;
+
 exec $SHELL -l
+
