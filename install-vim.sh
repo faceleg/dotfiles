cbea6962f249d33e7fed721771ac27c529700c45
Michael Robinson <mike@panmedia.co.nz>
2013-04-29 09:11:05 +1200

    More plugins, all the plugins

--- install-vim.sh
+++ install-vim.sh
@@ -12,8 +12,15 @@ $INSTALLER fholgado minibufexpl.vim
 $INSTALLER kien ctrlp.vim
 $INSTALLER scrooloose syntastic
 $INSTALLER scrooloose nerdtree
+$INSTALLER scrooloose nerdcommenter
 $INSTALLER terryma vim-multiple-cursors
 $INSTALLER majutsushi tagbar
 # requres https://github.com/techlivezheng/phpctags
 #$INSTALLER techlivezheng tagbar-phpctags 
 $INSTALLER xolox vim-easytags
+$INSTALLER skammer vim-css-color
+$INSTALLER SirVer ultisnips
+$INSTALLER JazzCore neocomplcache-ultisnips
+$INSTALLER tristen vim-sparkup
+$INSTALLER Townk vim-autoclose
+$INSTALLER Lokaltog vim-powerline
