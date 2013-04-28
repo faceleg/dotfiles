7967c89799e197de99b8acb4e68ce3e3bb9886cf
Michael Robinson <mike@panmedia.co.nz>
2013-04-29 10:34:39 +1200

    Updated vim installer

--- install-vim.sh
+++ install-vim.sh
@@ -16,7 +16,7 @@ $INSTALLER scrooloose nerdcommenter
 $INSTALLER terryma vim-multiple-cursors
 $INSTALLER majutsushi tagbar
 # requres https://github.com/techlivezheng/phpctags
-#$INSTALLER techlivezheng tagbar-phpctags 
+#$INSTALLER techlivezheng tagbar-phpctags
 $INSTALLER xolox vim-easytags
 $INSTALLER skammer vim-css-color
 $INSTALLER SirVer ultisnips
@@ -25,4 +25,17 @@ $INSTALLER tristen vim-sparkup
 $INSTALLER Townk vim-autoclose
 $INSTALLER Lokaltog vim-powerline
 $INSTALLER tobyS pdv
+$INSTALLER pangloss vim-javascript
+$INSTALLER sickill vim-sunburst
+
+# https://github.com/jelera/vim-javascript-syntax
+echo "Installing javascript syntax"
+mkdir -p ~/.vim/syntax
+curl -0 https://raw.github.com/jelera/vim-javascript-syntax/master/syntax/javascript.vim > ~/.vim/syntax/javascript.vim
+
+mkdir -p ~/.vim/ftdetect
+curl -0 https://raw.github.com/jelera/vim-javascript-syntax/master/ftdetect/javascript.vim > ~/.vim/ftdetect/javascript.vim
+echo ""
+
+# cp vim/sytnax/javascript.vim ~/.vim/syntax/javascript.vim
 
