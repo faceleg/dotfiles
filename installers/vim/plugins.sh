f629578d52b1af78e3fea67c6b1aad1a2ce95c5c
Michael Robinson <mike@pagesofinterest.net>
2013-04-30 23:39:15 +1200

    Delete plugin dir on install

--- installers/vim/plugins.sh
+++ installers/vim/plugins.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #CP="/bin/cp -vfr"
-#
-#printf "\nInstalling vim plugins\n"
-#
+
+printf "\nInstalling vim plugins\n"
+
+rm -rfv ~/.vim/plugin
 #mkdir -p ~/.vim/plugin
 #PLUGINS_DIR="`eval echo ${DIR//>}/../../addons/vim/plugin`"
 #$CP $PLUGINS_DIR ~/.vim
