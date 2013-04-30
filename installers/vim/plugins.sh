9d6e96622597b0693570e6d3ea95ec3003284214
Michael Robinson <mike@pagesofinterest.net>
2013-04-30 23:37:33 +1200

    Removed buffer delete plugin

--- installers/vim/plugins.sh
+++ installers/vim/plugins.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CP="/bin/cp -vfr"
-
-printf "\nInstalling vim plugins\n"
-
-mkdir -p ~/.vim/plugin
-PLUGINS_DIR="`eval echo ${DIR//>}/../../addons/vim/plugin`"
-$CP $PLUGINS_DIR ~/.vim
+#DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#CP="/bin/cp -vfr"
+#
+#printf "\nInstalling vim plugins\n"
+#
+#mkdir -p ~/.vim/plugin
+#PLUGINS_DIR="`eval echo ${DIR//>}/../../addons/vim/plugin`"
+#$CP $PLUGINS_DIR ~/.vim
 
