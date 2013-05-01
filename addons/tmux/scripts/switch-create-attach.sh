48229fbaeac71e8939d2fe726dfb094c52677aa3
Michael Robinson <mike@pagesofinterest.net>
2013-05-01 22:38:41 +1200

    Added tmux script to attach, create or switch to a named session

---
+++ addons/tmux/scripts/switch-create-attach.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+
+# Switch to previous session if no session name is provided
+if [[ -z $1 ]]; then
+  tmux switch-client -l
+else
+  # If there is a session with this name, attach or switch to it
+  if tmux has-session -t "$1" 2>/dev/null; then
+    [[ -z "$TMUX" ]] && tmux attach -t "$1" || tmux switch-client -t "$1"
+  # Otherwise create or attach it
+  else
+    [[ -z "$TMUX" ]] && tmux new-session -s "$1" || tmux attach -t "$1"
+ fi
+fi
+
