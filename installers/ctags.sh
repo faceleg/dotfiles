ae044b1175407b57a25afe1c1fb4346f398769e7
Michael Robinson <mike@panmedia.co.nz>
2013-05-06 11:11:14 +1200

    Updated ctags installer

--- installers/ctags.sh
+++ installers/ctags.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# Install ctags patched to provide css support
+
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 printf "\nInstalling ctags\n"
 
+# Don't attempt to install if ctags exists already
 if hash ctags 2>/dev/null; then
     printf "\nCTags is installed, remove it before attempting to install again\n"
     exit 1
@@ -13,7 +16,9 @@ mkdir -p ~/.ctags-src
 
 cd ~/.ctags-src
 
+# Clone the deploy branch
 git clone -b deploy https://github.com/fishman/ctags.git ~/.ctags-src
 
-./configure && make && sudo make install
+# Configure & build
+./configure && make && sudo make install && sudo ln -s -f /usr/local/bin/ctags /usr/bin/ctags-exuberant
 
