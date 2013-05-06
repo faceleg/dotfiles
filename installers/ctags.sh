6c9cd15ae77077cb5257033a7c885280d208a14b
Michael Robinson <mike@pagesofinterest.net>
2013-05-06 21:44:45 +1200

    Install ctags always

--- installers/ctags.sh
+++ installers/ctags.sh
@@ -5,12 +5,6 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 printf "\nInstalling ctags\n"
 
-# Don't attempt to install if ctags exists already
-if hash ctags 2>/dev/null; then
-    printf "\nCTags is installed, remove it before attempting to install again\n"
-    exit 1
-fi
-
 rm -rf ~/.ctags-src
 mkdir -p ~/.ctags-src
 
