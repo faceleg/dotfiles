80b2fe5443bf59e232d2c893835607bff2dca335
Michael Robinson <mike@panmedia.co.nz>
2013-05-08 09:00:33 +1200

    Added numbers, sorted plugins

--- installers/vim.sh
+++ installers/vim.sh
@@ -11,44 +11,46 @@ $CP "$DIR/../configuration/vimrc" ~/.vimrc
 printf "\nInstalling vim bundles\n"
 
 INSTALLER="$DIR/vim/bundle.sh"
+
 $INSTALLER airblade vim-gitgutter
-$INSTALLER tpope vim-fugitive
-$INSTALLER tpope vim-surround
-$INSTALLER tpope vim-unimpaired
-$INSTALLER tpope vim-haml
-$INSTALLER tpope vim-eunuch                         # https://github.com/tpope/vim-eunuch
-$INSTALLER Shougo neocomplcache
+$INSTALLER ap vim-css-color
+$INSTALLER flazz vim-colorschemes
+$INSTALLER heavenshell vim-jsdoc
+$INSTALLER hlissner vim-multiedit
+$INSTALLER JazzCore neocomplcache-ultisnips
+$INSTALLER jeetsukumaran vim-buffergator
 $INSTALLER kien ctrlp.vim
-$INSTALLER scrooloose syntastic
-$INSTALLER scrooloose nerdtree
+$INSTALLER Lokaltog vim-easymotion
+$INSTALLER Lokaltog vim-powerline
+$INSTALLER majutsushi tagbar
+$INSTALLER myusuf3 numbers.vim                      # https://github.com/myusuf3/numbers.vim
+$INSTALLER othree javascript-libraries-syntax.vim
+$INSTALLER pangloss vim-javascript
+$INSTALLER plasticboy vim-markdown
 $INSTALLER scrooloose nerdcommenter
+$INSTALLER scrooloose nerdtree
+$INSTALLER scrooloose syntastic
+$INSTALLER Shougo neocomplcache
+$INSTALLER sickill vim-sunburst
+$INSTALLER SirVer ultisnips
+$INSTALLER sjl gundo.vim
+$INSTALLER terryma vim-expand-region                # https://github.com/terryma/vim-expand-region
 $INSTALLER terryma vim-multiple-cursors
 $INSTALLER terryma vim-smooth-scroll                # https://github.com/terryma/vim-smooth-scroll
-$INSTALLER terryma vim-expand-region                # https://github.com/terryma/vim-expand-region
-$INSTALLER majutsushi tagbar
-$INSTALLER xolox vim-easytags
-$INSTALLER ap vim-css-color
-$INSTALLER SirVer ultisnips
-$INSTALLER JazzCore neocomplcache-ultisnips
-$INSTALLER tristen vim-sparkup
+$INSTALLER tobyS pdv
 $INSTALLER Townk vim-autoclose                      # https://github.com/Townk/vim-autoclose
+$INSTALLER tpope vim-eunuch                         # https://github.com/tpope/vim-eunuch
+$INSTALLER tpope vim-fugitive
+$INSTALLER tpope vim-haml
+$INSTALLER tpope vim-surround
+$INSTALLER tpope vim-unimpaired
+$INSTALLER tristen vim-sparkup
+$INSTALLER Valloric MatchTagAlways
+$INSTALLER Valloric vim-indent-guides
 $INSTALLER vim-scripts closetag.vim                 # https://github.com/vim-scripts/closetag.vim
 $INSTALLER vim-scripts phpfolding.vim
 $INSTALLER vim-scripts YankRing.vim
-$INSTALLER Lokaltog vim-powerline
-$INSTALLER Lokaltog vim-easymotion
-$INSTALLER tobyS pdv
-$INSTALLER pangloss vim-javascript
-$INSTALLER sickill vim-sunburst
-$INSTALLER othree javascript-libraries-syntax.vim
-$INSTALLER hlissner vim-multiedit
-$INSTALLER Valloric vim-indent-guides
-$INSTALLER Valloric MatchTagAlways
-$INSTALLER flazz vim-colorschemes
-$INSTALLER sjl gundo.vim
-$INSTALLER heavenshell vim-jsdoc
-$INSTALLER jeetsukumaran vim-buffergator
-$INSTALLER plasticboy vim-markdown
+$INSTALLER xolox vim-easytags
 
 if [[ -d "`eval echo ~/.phpctags/vendor/techlivezheng/phpctags`" ]]; then
     # Use phpctags.sh installer first
