3618df0085992002b8539e239d432df99aa9158f
Michael Robinson <mike@pagesofinterest.net>
2013-05-20 11:23:10 +1200

    re-added neocomplcache, removed youcompleteme

--- installers/vim.sh
+++ installers/vim.sh
@@ -12,30 +12,34 @@ printf "\nInstalling vim bundles\n"
 
 INSTALLER="$DIR/vim/bundle.sh"
 
+# $INSTALLER flazz vim-colorschemes
+# $INSTALLER nelstrom vim-visual-star-search          # https://github.com/nelstrom/vim-visual-star-search
+# $INSTALLER othree javascript-libraries-syntax.vim
+# $INSTALLER Valloric YouCompleteMe                   # https://github.com/Valloric/YouCompleteMe
 $INSTALLER airblade vim-gitgutter
 $INSTALLER ap vim-css-color
-# $INSTALLER flazz vim-colorschemes
 $INSTALLER hail2u vim-css3-syntax                   # https://github.com/hail2u/vim-css3-syntax
 $INSTALLER heavenshell vim-jsdoc
 $INSTALLER hlissner vim-multiedit
+$INSTALLER JazzCore neocomplcache-ultisnips
 $INSTALLER jeetsukumaran vim-buffergator
 $INSTALLER kien ctrlp.vim
 $INSTALLER Lokaltog vim-easymotion
 $INSTALLER Lokaltog vim-powerline
 $INSTALLER majutsushi tagbar
 $INSTALLER mudpile45 vim-phpdoc                     # https://github.com/mudpile45/vim-phpdoc
-# $INSTALLER nelstrom vim-visual-star-search          # https://github.com/nelstrom/vim-visual-star-search
-# $INSTALLER othree html5.vim                         # https://github.com/othree/html5.vim
-# $INSTALLER othree javascript-libraries-syntax.vim
+$INSTALLER othree html5.vim                         # https://github.com/othree/html5.vim
 $INSTALLER pangloss vim-javascript
 $INSTALLER plasticboy vim-markdown
+$INSTALLER rodjek vim-puppet                        # https://github.com/rodjek/vim-puppet
 $INSTALLER scrooloose nerdtree
 $INSTALLER scrooloose syntastic
+$INSTALLER Shougo neocomplcache
 $INSTALLER sickill vim-sunburst
+$INSTALLER SirVer ultisnips                         # https://github.com/SirVer/ultisnips
 $INSTALLER sjl gundo.vim
 $INSTALLER terryma vim-expand-region                # https://github.com/terryma/vim-expand-region
 $INSTALLER terryma vim-multiple-cursors
-$INSTALLER SirVer ultisnips                         # https://github.com/SirVer/ultisnips
 $INSTALLER terryma vim-smooth-scroll                # https://github.com/terryma/vim-smooth-scroll
 $INSTALLER tobyS pdv
 $INSTALLER tobyS vmustache
@@ -47,10 +51,8 @@ $INSTALLER tpope vim-haml
 $INSTALLER tpope vim-surround
 $INSTALLER tpope vim-unimpaired
 $INSTALLER tristen vim-sparkup
-$INSTALLER Valloric YouCompleteMe                   # https://github.com/Valloric/YouCompleteMe
 $INSTALLER vim-scripts closetag.vim                 # https://github.com/vim-scripts/closetag.vim
 $INSTALLER vim-scripts YankRing.vim
-$INSTALLER rodjek vim-puppet                        # https://github.com/rodjek/vim-puppet
 
 if [[ -d "`eval echo ~/.phpctags/vendor/techlivezheng/phpctags`" ]]; then
     # Use phpctags.sh installer first
@@ -73,8 +75,4 @@ if [[ ! -d $JSCTAGS_DIR ]]; then
     cd jsctags
     sudo make install
 fi
-
-# printf "\nInstalling YouCompleteMe\n"
-# cd ~/.vim/bundle/YouCompleteMe
-# ./install.sh --clang-completer
 
