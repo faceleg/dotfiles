611a54dff1f495035d7f5acb75b2bb260d6ac664
Michael Robinson <mike@pagesofinterest.net>
2013-07-09 10:43:30 +1200

    removed ctags from vim installer

--- installers/vim.sh
+++ installers/vim.sh
@@ -17,23 +17,6 @@ if [ ! -f ~/.vim/syntax/javascript.vim ]; then
     curl -0 https://raw.github.com/jelera/vim-javascript-syntax/master/ftdetect/javascript.vim > ~/.vim/ftdetect/javascript.vim
 fi
 
-# Install patched version of Ctags for PHP if not present
-CTAGS_VERSION=$(ctags --version)
-if [[ ! $CTAGS_VERSION =~ 'Exuberant Ctags Development' ]]; then
-    mkdir -p ~/.vim/build
-    cd ~/.vim/build
-    svn --non-interactive --trust-server-cert --quiet checkout https://svn.code.sf.net/p/ctags/code/trunk ~/.vim/build/ctags-trunk
-    cd ~/.vim/build/ctags-trunk
-    wget --quiet https://gist.github.com/complex857/5693196/raw/14a770b436b5116eb22ec65492d8c0e8a4271210/0000-PHP-parser-rewrite-full-string-parameters.patch
-    patch -p0 --quiet -i 0000-PHP-parser-rewrite-full-string-parameters.patch
-    autoconf
-    autoheader
-    ./configure
-    make
-    sudo make install
-fi
-
-
 JSCTAGS_DIR=/usr/local/jsctags
 if [[ ! -d $JSCTAGS_DIR ]]; then
     printf "\nInstalling jsctags\n"
