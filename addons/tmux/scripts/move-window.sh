60cf5463bbe59cb93b1286b7d9ab42e90571f5f6
Michael Robinson <mike@pagesofinterest.net>
2014-07-04 13:21:56 +1200

    Removed jsctags installer & custom js syntax installer that I don't remember anymore

--- addons/tmux/scripts/move-window.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $# -ne 1 -o -z "$1" ]; then
-    exit 1
-fi
-if tmux list-windows | grep -q "^$1:"; then
-    tmux swap-window -t $1
-else
-    tmux move-window -t $1
-fi
--- installers/vim.sh
+++ installers/vim.sh
@@ -7,26 +7,6 @@ $CP "$DIR/../configuration/vimrc" ~/.vimrc
 
 "$DIR/vim/neobundle.sh"
 
-if [ ! -f ~/.vim/syntax/javascript.vim ]; then
-    # https://github.com/jelera/vim-javascript-syntax
-    printf "\nInstalling javascript syntax\n"
-    mkdir -p ~/.vim/syntax
-    curl -0 https://raw.github.com/jelera/vim-javascript-syntax/master/syntax/javascript.vim > ~/.vim/syntax/javascript.vim
-
-    mkdir -p ~/.vim/ftdetect
-    curl -0 https://raw.github.com/jelera/vim-javascript-syntax/master/ftdetect/javascript.vim > ~/.vim/ftdetect/javascript.vim
-fi
-
-JSCTAGS_DIR=/usr/local/jsctags
-if [[ ! -d $JSCTAGS_DIR ]]; then
-    printf "\nInstalling jsctags\n"
-    cd /usr/local
-    #sudo git clone --recursive https://github.com/Powpow-Shen/doctorjs jsctags
-    sudo git clone --recursive https://github.com/yarray/doctorjs jsctags
-    cd jsctags
-    sudo make install
-fi
-
 # CSSLint for Syntastic
 command_exists () {
     type "$1" &> /dev/null ;
