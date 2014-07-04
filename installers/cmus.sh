0ee993edcf53740a6ccf3738111fad7fbe1c8fbf
Michael Robinson <mike@pagesofinterest.net>
2014-07-04 13:18:02 +1200

    Removed cmus

--- installers/cmus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CP="/bin/cp -vfr"
-
-printf "\nConfiguring cmus\n"
-
-# Install tmux themes
-$CP "$DIR/../addons/cmus/." ~/.cmus/
-
