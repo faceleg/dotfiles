e183a2888454bdcc33a5b0b05008fc88c1bf90ae
Michael Robinson <mike@pagesofinterest.net>
2014-07-04 13:18:52 +1200

    Removed git config

--- installers/git-configuration.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-printf "\nConfiguring git\n"
-
-# Git config
-git config --global --add color.ui true
-git config --global core.editor "vim"
-git config --global help.autocorrect 30
-
