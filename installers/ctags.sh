a192afb80fc144ff91183446eadbfd7b4f37c654
Michael Robinson <mike@pagesofinterest.net>
2014-07-07 09:56:30 +1200

    Updated ctags installer

--- installers/ctags.sh
+++ installers/ctags.sh
@@ -5,14 +5,6 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 printf "\nInstalling ctags\n"
 
-rm -rf ~/.ctags-src
-mkdir -p ~/.ctags-src
-
-cd ~/.ctags-src
-
-# Clone the deploy branch
-git clone -b deploy https://github.com/faceleg/ctags.git ~/.ctags-src
-
-# Configure & build
-./configure && make && sudo make install && sudo ln -s -f /usr/local/bin/ctags /usr/bin/ctags-exuberant
+brew tap kopischke/ctags
+brew install ctags-fishman --HEAD
 
