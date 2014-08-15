0e7caa91c8abaa8018f6cf0ba4978fc1b4421daf
Michael Robinson <mike@pagesofinterest.net>
2014-08-15 20:59:44 +1200

    Updated ctags, then stopped using them again.

--- installers/ctags.sh
+++ installers/ctags.sh
@@ -5,6 +5,26 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 printf "\nInstalling ctags\n"
 
-brew tap kopischke/ctags
-brew install ctags-fishman --HEAD
+rm -rfv ~/.ctags-src
+mkdir -vp ~/.ctags-src
+
+cd ~/.ctags-src
+
+# Clone the deploy branch
+git clone -b deploy https://github.com/fishman/ctags.git ~/.ctags-src
+
+# Configure & build
+autoheader
+autoconf
+./configure
+
+make
+sudo make install
+sudo ln -s -f /usr/local/bin/ctags /usr/bin/ctags-exuberant
+
+# brew tap kopischke/ctags
+# brew install ctags-fishman --HEAD
+
+# brew install ctags --HEAD
+# brew link --overwrite ctags
 
