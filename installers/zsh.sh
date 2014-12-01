51dd406e382850b126a4c5054cb840f297e3c75d
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-12-02 10:02:20 +1300

    Use https version of zsh theme

--- installers/zsh.sh
+++ installers/zsh.sh
@@ -18,7 +18,7 @@ ZSH_SYNTAX="$DIRECTORY/zsh-syntax-highlighting"
 
 if [ ! -d "`eval echo ${ZSH_SYNTAX//>}`" ]; then
     cd ~/.oh-my-zsh/custom/plugins
-    git clone git://github.com/zsh-users/zsh-syntax-highlighting.git zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git zsh-syntax-highlighting
 else
     cd ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
     git pull
