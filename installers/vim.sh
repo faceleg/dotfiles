d59f56aa5d48c0510bb229d517299aa7d79878b1
Michael Robinson <michael.robinson@kiwirail.co.nz>
2014-12-12 14:59:41 +1300

    neobundle will install from vimrc

--- installers/vim.sh
+++ installers/vim.sh
@@ -2,10 +2,7 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CP="/bin/cp -vfr"
 
-#$CP "$DIR/../configuration/ctags" ~/.ctags
 $CP "$DIR/../configuration/vimrc" ~/.vimrc
-
-"$DIR/vim/neobundle.sh"
 
 # CSSLint for Syntastic
 command_exists () {
