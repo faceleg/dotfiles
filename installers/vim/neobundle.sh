910850452cf5c618051a77174e7e320e7bfd14d2
Michael Robinson <michael.robinson@kiwirail.co.nz>
2015-01-09 15:18:09 +1300

    Removed neobundle.sh script

--- installers/vim/neobundle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DIRECTORY="`eval echo ~/.vim/bundle/neobundle.vim`"
-
-if [ ! -d "$DIRECTORY" ]; then
-    printf "\nInstalling neobundle\n"
-    mkdir -p ~/.vim/bundle "$DIRECTORY"; \
-    git clone https://github.com/Shougo/neobundle.vim.git "$DIRECTORY"
-fi
-
