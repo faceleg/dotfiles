3192470eb8a9af91fcb5c68a267ca4bb613597a2
Michael Robinson <michael.robinson@kiwirail.co.nz>
2015-01-09 15:18:20 +1300

    Also make an nvimrc file when installing vim

--- installers/vim.sh
+++ installers/vim.sh
@@ -3,6 +3,7 @@ DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CP="/bin/cp -vfr"
 
 $CP "$DIR/../configuration/vimrc" ~/.vimrc
+$CP ~/.vimrc ~/.nvimrc
 
 # CSSLint for Syntastic
 command_exists () {
