136d7bc3f707c60bb31559f2aa76cdb97983428c
Michael Robinson <michael.robinson@solnet.co.nz>
2015-07-27 10:31:43 +1200

    Added git aliases

--- configuration/config.fish
+++ configuration/config.fish
@@ -42,3 +42,45 @@ end
 # Base16 Shell
 eval sh $HOME/.config/base16-shell/base16-colors.dark.sh
 
+# Aliases
+
+################################
+###  Program ShortCut
+################################
+
+# git related shortcut
+alias undopush="git push -f origin HEAD^:master"
+alias gd="git diff"
+alias gdc="git diff --cached"
+alias ga="git add"
+alias gca="git commit -a -m"
+alias gcm="git commit -m"
+alias gbd="git branch -D"
+alias gst="git status -sb --ignore-submodules"
+alias gm="git merge --no-ff"
+alias gpt="git push --tags"
+alias gp="git push"
+alias grs="git reset --soft"
+alias grh="git reset --hard"
+alias gb="git branch"
+alias gcob="git checkout -b"
+alias gco="git checkout"
+alias gba="git branch -a"
+alias gcp="git cherry-pick"
+alias gl="git lg"
+alias gpom="git pull origin master"
+# turn on coloring on grep
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+# Quick check running state of a process
+alias psef="ps -ef | grep "
+alias top="top -o cpu"
+alias ps="command ps -SAcr -o 'uid,pid,ppid,time,rss,command'"
+
+if [ -d "$ANTIGEN/.git" ]; thenrgv; end
+function tree2; tree --dirsfirst -ChFLQ 2 $argv; end
+function tree3; tree --dirsfirst -ChFLQ 3 $argv; end
+function tree4; tree --dirsfirst -ChFLQ 4 $argv; end
+function tree5; tree --dirsfirst -ChFLQ 5 $argv; end
+function tree6; tree --dirsfirst -ChFLQ 6 $argv; end
