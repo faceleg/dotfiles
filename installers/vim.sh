112ed7f0a2c8298abd13565a3d83f40cc7c63409
Michael Robinson <michael.robinson@kiwirail.co.nz>
2015-11-11 14:45:32 +1300

    Updated vim config installer to be compatible with updated neovim

--- installers/vim.sh
+++ installers/vim.sh
@@ -2,6 +2,11 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CP="/bin/cp -vfr"
 
+# Vim
 $CP "$DIR/../configuration/vimrc" ~/.vimrc
-$CP ~/.vimrc ~/.nvimrc
+
+# Neovim
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+mkdir -p "$XDG_CONFIG_HOME/nvim"
+$CP "$DIR/../configuration/vimrc" "$XDG_CONFIG_HOME/nvim/init.vim"
 
