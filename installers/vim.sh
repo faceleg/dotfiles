6a56a95f7d0abd371443369de08f3fb32fc13e42
Michael Robinson <michael.robinson@solnet.co.nz>
2016-04-11 14:08:19 +1200

    Install separate plugin files

--- installers/vim.sh
+++ installers/vim.sh
@@ -2,13 +2,17 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CP="/bin/cp -vfr"
 
-# Vim
-$CP "$DIR/../configuration/vimrc" ~/.vimrc
-
 # Neovim
 mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
-mkdir -p "$XDG_CONFIG_HOME/nvim"
+
+VIM_DIR_NAME="nvim"
+
+echo "mkdir $XDG_CONFIG_HOME/$VIM_DIR_NAME/config"
+mkdir -p "$XDG_CONFIG_HOME/$VIM_DIR_NAME/config"
+
 $CP "$DIR/../configuration/vimrc" "$XDG_CONFIG_HOME/nvim/init.vim"
 if [ ! -f  "$XDG_CONFIG_HOME/nvim/github.vim" ]; then
-    $CP "$DIR/../configuration/vimrc.github.vim" "$XDG_CONFIG_HOME/nvim/github.vim"
+    $CP "$DIR/../configuration/vimrc.github.vim" "$XDG_CONFIG_HOME/$VIM_DIR_NAME/config/github.vim"
 fi
+
+$CP "$DIR/../configuration/vimrc.plugins.vim" "$XDG_CONFIG_HOME/$VIM_DIR_NAME/config/plugins.vim"
