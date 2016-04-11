69e7ea8c1e551e83fb7364a68b2e5280b4277133
Michael Robinson <mike@pagesofinterest.net>
2016-04-11 23:38:42 +1200

    Fixed vim config dir

--- installers/vim.sh
+++ installers/vim.sh
@@ -11,7 +11,7 @@ echo "mkdir $XDG_CONFIG_HOME/$VIM_DIR_NAME/config"
 mkdir -p "$XDG_CONFIG_HOME/$VIM_DIR_NAME/config"
 
 $CP "$DIR/../configuration/vimrc" "$XDG_CONFIG_HOME/nvim/init.vim"
-if [ ! -f  "$XDG_CONFIG_HOME/nvim/github.vim" ]; then
+if [ ! -f  "$XDG_CONFIG_HOME/nvim/config/github.vim" ]; then
     $CP "$DIR/../configuration/vimrc.github.vim" "$XDG_CONFIG_HOME/$VIM_DIR_NAME/config/github.vim"
 fi
 
