7f2c4f460779a7260137d4eece9b908070ce8fa1
Michael Robinson <michael.robinson@solnet.co.nz>
2016-05-05 11:24:10 +1200

    Removed github config

--- configuration/vimrc.github.vim
+++
@@ -1,3 +0,0 @@
-" Populate AFTER installation
-" Used for https://github.com/jaxbot/github-issues.vim
-let g:github_access_token = ""
--- installers/vim.sh
+++ installers/vim.sh
@@ -11,9 +11,6 @@ echo "mkdir $XDG_CONFIG_HOME/$VIM_DIR_NAME/config"
 mkdir -p "$XDG_CONFIG_HOME/$VIM_DIR_NAME/config"
 
 $CP "$DIR/../configuration/vimrc" "$XDG_CONFIG_HOME/nvim/init.vim"
-if [ ! -f  "$XDG_CONFIG_HOME/nvim/config/github.vim" ]; then
-    $CP "$DIR/../configuration/vimrc.github.vim" "$XDG_CONFIG_HOME/$VIM_DIR_NAME/config/github.vim"
-fi
 
 $CP "$DIR/../configuration/vimrc.plugins.vim" "$XDG_CONFIG_HOME/$VIM_DIR_NAME/config/plugins.vim"
 $CP "$DIR/../configuration/vimrc.plugins.config.vim" "$XDG_CONFIG_HOME/$VIM_DIR_NAME/config/plugins.config.vim"
