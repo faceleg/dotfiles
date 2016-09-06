4b04adb066cae92b424488b9558fc3afbebee201
Michael Robinson <mike@pagesofinterest.net>
2016-09-06 16:09:27 +1200

    Move old backup to side before backing up new intellij filesc

--- intellijidea-backup.sh
+++ intellijidea-backup.sh
@@ -2,33 +2,36 @@
 
 source ./intellijidea-variables.sh
 
+DATE=`date +%Y-%m-%d_%H-%M-%S`
+mv ${BACKUP} "${BACKUP}__${DATE}"
+
 mkdir -p "$BACKUP/configuration"
 mkdir -p "$BACKUP/plugins"
 
 $CP ~/Library/Preferences/${PRODUCT}/* "$BACKUP/configuration/"
-rm -rf $BACKUP/configuration/port*
-rm -rf $BACKUP/configuration/tasks
-rm -rf $BACKUP/configuration/settingsRepository
-rm -rf $BACKUP/configuration/jdbc-drivers
-rm -rf $BACKUP/configuration/eval
-rm -rf $BACKUP/configuration/options/git.xml
-rm -rf $BACKUP/configuration/options/dimensions.xml
-rm -rf $BACKUP/configuration/options/feature.usage.statistics.xml
-rm -rf $BACKUP/configuration/options/jdk.table.xml
-rm -rf $BACKUP/configuration/options/window.manager.xml
-rm -rf $BACKUP/configuration/options/window.state.xml
-rm -rf $BACKUP/configuration/options/options.xml
-rm -rf $BACKUP/configuration/options/databaseDrivers.xml
-rm -rf $BACKUP/configuration/options/databaseSettings.xml
-rm -rf $BACKUP/configuration/options/proxy*
-rm -rf $BACKUP/configuration/options/recentProjects.xml
-rm -rf $BACKUP/configuration/options/other.xml
-rm -rf $BACKUP/configuration/options/security.xml
-rm -rf $BACKUP/configuration/options/statistics.application.usages.xml
-rm -rf $BACKUP/configuration/options/stubIndex.xml
-rm -rf $BACKUP/configuration/options/updates.xml
-rm -rf $BACKUP/configuration/options/usage.statistics.xml
-rm -rf $BACKUP/configuration/options/vim_settings.xml
+rm -rf ${BACKUP}/configuration/port*
+rm -rf ${BACKUP}/configuration/tasks
+rm -rf ${BACKUP}/configuration/settingsRepository
+rm -rf ${BACKUP}/configuration/jdbc-drivers
+rm -rf ${BACKUP}/configuration/eval
+rm -rf ${BACKUP}/configuration/options/git.xml
+rm -rf ${BACKUP}/configuration/options/dimensions.xml
+rm -rf ${BACKUP}/configuration/options/feature.usage.statistics.xml
+rm -rf ${BACKUP}/configuration/options/jdk.table.xml
+rm -rf ${BACKUP}/configuration/options/window.manager.xml
+rm -rf ${BACKUP}/configuration/options/window.state.xml
+rm -rf ${BACKUP}/configuration/options/options.xml
+rm -rf ${BACKUP}/configuration/options/databaseDrivers.xml
+rm -rf ${BACKUP}/configuration/options/databaseSettings.xml
+rm -rf ${BACKUP}/configuration/options/proxy*
+rm -rf ${BACKUP}/configuration/options/recentProjects.xml
+rm -rf ${BACKUP}/configuration/options/other.xml
+rm -rf ${BACKUP}/configuration/options/security.xml
+rm -rf ${BACKUP}/configuration/options/statistics.application.usages.xml
+rm -rf ${BACKUP}/configuration/options/stubIndex.xml
+rm -rf ${BACKUP}/configuration/options/updates.xml
+rm -rf ${BACKUP}/configuration/options/usage.statistics.xml
+rm -rf ${BACKUP}/configuration/options/vim_settings.xml
 
 $CP ~/Library/Application\ Support/${PRODUCT}/* "$BACKUP/plugins/"
 
