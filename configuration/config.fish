c7ea3d179dda384f93b3361155fe935fb32a4993
Michael Robinson <mike@pagesofinterest.net>
2017-01-12 23:11:22 +1300

    (fish) Added git aliases

--- configuration/config.fish
+++ configuration/config.fish
@@ -8,3 +8,21 @@ end
 function fish_mode_prompt; end
 
 set -g fish_key_bindings fish_vi_key_bindings
+
+##----GIT------
+alias gs='clear ;and git status'
+alias gb='git branch'
+alias gbranch='git rev-parse --abbrev-ref HEAD' #get current branch name
+alias gl="clear ;and git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' b--abbrev-commit"
+alias gt='git tag'
+alias grm='git rm'
+alias gps='git push'
+alias gbi='git bisect'
+alias gbg='git bisect good'
+alias gbb='git bisect bad'
+alias gco='git checkout'
+alias gp='git pull'
+alias gr='git rebase'
+alias gri='git rebase -i'
+alias gst='git status'
+alias g='git'
