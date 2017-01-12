18ec7f9429fd47ebbb275761eb18f674adc0b4c0
Michael Robinson <mike@pagesofinterest.net>
2017-01-12 23:10:59 +1300

    (fish) Fixed install

--- installers/fish.sh
+++ installers/fish.sh
@@ -7,10 +7,7 @@ CP="/bin/cp -vfr"
 printf "\Installing Base16 Colors"
 git clone https://github.com/chriskempson/base16-shell ~/.config/base16-shell
 
-printf "\Installing Fisherman"
-curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisher
-
 printf "\nReplacing fish.config\n"
 rm -fv ~/.config/fish.config
 $CP "$DIR/../configuration/fishfile" ~/.config/fish/fishfile
-$CP "$DIR/../configuration/fish" ~/.config/fish/conf.d/fish
+$CP "$DIR/../configuration/config.fish" ~/.config/fish/config.fish
