7e5e68f83896c6536be826ec519ee73d4eb780c7
Michael Robinson <mike@pagesofinterest.net>
2017-01-12 22:19:02 +1300

    (chore) Removed various unused configs

--- installers/ctags.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-printf "\nInstalling ctags configuration\n"
-
-rm -rfv ~/.ctags
-
-cp -v "$DIR/../configuration/ctags" ~/.ctags
