048c58ac2958f8244ede5165a060b703f56ecbbe
Michael Robinson <michael.robinson@solnet.co.nz>
2017-01-13 09:30:15 +1300

    (fish) Make fish config dir on install

--- installers/fish.sh
+++ installers/fish.sh
@@ -9,6 +9,7 @@ git clone https://github.com/chriskempson/base16-shell ~/.config/base16-shell
 
 printf "\nReplacing fish.config\n"
 rm -fv ~/.config/fish.config
+mkdir -p ~/.config/fish
 $CP "$DIR/../configuration/fishfile" ~/.config/fish/fishfile
 $CP "$DIR/../configuration/config.fish" ~/.config/fish/config.fish
 $CP "$DIR/../configuration/logo.txt" ~/.config/fish/logo.txt
