2c222c96cd73fc15af5341abae440ce2eb84ec0a
Michael Robinson <mike@pagesofinterest.net>
2017-02-15 19:58:49 +1300

    (tmux) Removed invalid options, added comment about ftpane

--- installers/tmux.sh
+++ installers/tmux.sh
@@ -6,7 +6,9 @@ rm -f ~/.tmux.conf ~/.tmux.conf.local
 $CP "$DIR/../configuration/tmux.conf" ~/.tmux.conf
 
 mkdir -p ~/.tmux
-curl https://gist.githubusercontent.com/muralisc/d1ed7226533ab23e4e1e/raw/1291524f073ac742ca9cba211d229bb457acf90c/ftpane > ~/.tmux/ftpane.sh
+
+# Script for selecting tmux pane using fzf
+curl -v https://gist.githubusercontent.com/muralisc/d1ed7226533ab23e4e1e/raw/1291524f073ac742ca9cba211d229bb457acf90c/ftpane > ~/.tmux/ftpane.sh
 chmod +x ~/.tmux/ftpane.sh
 
 tmux source-file ~/.tmux.conf
