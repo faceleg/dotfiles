a28eb77029678ef9bd5232fb3d70412bea2638b5
Michael Robinson <mike@lightbox.co.nz>
2017-07-05 13:31:38 +1200

    (idea) Remove intellij scripts

--- intellijidea-restore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source ./intellijidea-variables.sh
-
-$CP "$BACKUP/configuration/" ~/Library/Preferences/${PRODUCT}/
-$CP "$BACKUP/plugins/" ~/Library/Application\ Support/${PRODUCT}/
-$CP "$BACKUP/ideavimrc" ~/.ideavimrc
--- intellijidea-variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-CP="/usr/bin/rsync -avhi"
-
-PRODUCT="IntelliJIdea2016.3"
-BACKUP="$HOME/Dropbox/$PRODUCT"
